028f69263867e98f115e453668e764462d05871b
nstrong <nstrong@spanimation.com>
2021-05-30 18:30:41 -0700

    linked lists

--- 3-linkedList/linkedList.js
+++ 3-linkedList/linkedList.js
@@ -4,6 +4,10 @@ class LinkedListNode {
         this.next = next;
     }
 
+    /**
+     * @param {Function} callback
+     * @returns {*|string}
+     */
     toString(callback = null) {
         return callback ? callback(this.value) : `${this.value}`;
     }
@@ -18,6 +22,10 @@ class LinkedList {
         this.tail = null;
     }
 
+    /**
+     * @param value
+     * @returns {LinkedList}
+     */
     prepend(value) {
         const newNode = new LinkedListNode(value, this.head);
         this.head = newNode;
@@ -25,6 +33,10 @@ class LinkedList {
         return this;
     }
 
+    /**
+     * @param value
+     * @returns {LinkedList}
+     */
     append(value) {
         const newNode = new LinkedListNode(value);
         if (!this.head) {
@@ -37,6 +49,10 @@ class LinkedList {
         return this;
     }
 
+    /**
+     * @param value
+     * @returns {LinkedListNode|null}
+     */
     delete(value) {
         if (!this.head) return null;
         let deletedNode = null;
@@ -50,9 +66,98 @@ class LinkedList {
         if (currentNode !== null) {
             while (currentNode.next) {
                 if (currentNode.next.value === value) {
-
+                    deletedNode = currentNode.next;
+                    currentNode.next = currentNode.next.next;
+                } else {
+                    currentNode = currentNode.next;
                 }
             }
         }
+
+        if (this.tail.value === value) {
+            this.tail = currentNode;
+        }
+
+        return deletedNode;
+    }
+
+    /**
+     * @returns {LinkedListNode}
+     */
+    deleteTail() {
+        const deletedTail = this.tail;
+
+        // If there is only one node in list..
+        if (this.head === this.tail) {
+            this.head = null;
+            this.tail = null;
+            return deletedTail;
+        }
+
+        // If there are multiple nodes in list,
+        // need to traverse entire list to set a new tail.
+        let currentNode = this.head;
+        while (currentNode.next) {
+            if (!currentNode.next.next) currentNode.next = null;
+            else currentNode = currentNode.next;
+        }
+        this.tail = currentNode;
+        return deletedTail;
+    }
+
+    /**
+     * @returns {LinkedListNode|null}
+     */
+    deleteHead() {
+        if (!this.head) return null;
+
+        const deletedHead = this.head;
+
+        if (this.head.next) this.head = this.head.next;
+        else {
+            // There was only one node in the list.. delete it.
+            this.head = null;
+            this.tail = null;
+        }
+        return deletedHead;
+    }
+
+    /**
+     * Takes either a value or a predicate (true | false) callback to compare values to find.
+     * @param value
+     * @param {Function} callback
+     * @returns {LinkedListNode|null}
+     */
+    find({value = undefined, callback = undefined}) {
+        if (!this.head) return null;
+
+        let currentNode = this.head;
+        while (currentNode) {
+            if (callback && callback(currentNode.value)) return currentNode;
+            if (value !== undefined && currentNode.value === value) return  currentNode;
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+
+    /**
+     * @returns {LinkedListNode[]}
+     */
+    toArray() {
+        const nodes = [];
+        let currentNode = this.head;
+        while (currentNode) {
+            nodes.push(currentNode);
+            currentNode = currentNode.next;
+        }
+        return nodes;
+    }
+
+    /**
+     * @param callback
+     * @returns {string}
+     */
+    toString(callback) {
+        return this.toArray().map(node => node.toString(callback)).toString()
     }
 }
