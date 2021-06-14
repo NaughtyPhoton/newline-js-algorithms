8e371b7f6bedafb219bee500ce14708200b85c54
nstrong <nstrong@spanimation.com>
2021-06-13 18:54:46 -0700

    isAPowerOfTwo

---
+++ 16-linearSearch/comparator.js
@@ -0,0 +1,75 @@
+/**
+ * @param {function(a: *, b: *)} [compareFunction] - Optionally a custom compare function.
+ * Otherwise the Comparator.defaultCompareFunction
+ */
+class Comparator {
+    constructor(comparatorFunction) {
+        this.compare = comparatorFunction || Comparator.defaultCompareFunction;
+    }
+
+    /**
+     * Default comparison function. It just assumes that "a" and "b" are strings or numbers.
+     * @param {(string|number)} a
+     * @param {(string|number)} b
+     * @returns {number}
+     */
+    static defaultCompareFunction(a, b) {
+        if (a === b) {
+            return 0;
+        }
+        return a < b ? -1 : 1;
+    }
+
+    /**
+     * Checks if two variables are equal.
+     * @param {*} a
+     * @param {*} b
+     * @return {boolean}
+     */
+    equal(a, b) {
+        return this.compare(a, b) === 0;
+    }
+
+    /**
+     * Checks if variable "a" is less than "b".
+     * @param {*} a
+     * @param {*} b
+     * @return {boolean}
+     */
+    lessThan(a, b) {
+        return this.compare(a, b) > 0;
+    }
+
+    /**
+     * Checks if variable "a" is less than or equal to "b".
+     * @param {*} a
+     * @param {*} b
+     * @return {boolean}
+     */
+    lessThanOrEqual(a, b) {
+        return this.lessThan(a, b) || this.equal(a, b);
+    }
+
+    /**
+     * Checks if variable "a" is greater than "b".
+     * @param {*} a
+     * @param {*} b
+     * @return {boolean}
+     */
+    greaterThan(a, b) {
+        return this.compare(a, b) > 0;
+    }
+
+    /**
+     * Checks if variable "a" is greater than or equal to "b".
+     * @param {*} a
+     * @param {*} b
+     * @return {boolean}
+     */
+    greaterThanOrEqual(a, b) {
+        return this.greaterThan(a, b) || this.equal(a, b);
+    }
+}
+
+
+module.exports = {Comparator};
---
+++ 16-linearSearch/linearSearch.js
@@ -0,0 +1,17 @@
+const {Comparator} = require("./comparator");
+
+function linearSearch(array, seekElement, comparatorCallback) {
+    const comparator = new Comparator(comparatorCallback);
+    const foundIndices = [];
+
+    array.forEach((element, index) => {
+        if (comparator.equal(element, seekElement)) {
+            foundIndices.push(index);
+        }
+    });
+
+    return foundIndices;
+}
+
+
+module.exports = {linearSearch};
