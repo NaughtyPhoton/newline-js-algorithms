340ce39d445bb4c2290ebd36b11d7641f1fd22a8
nstrong <nstrong@spanimation.com>
2021-06-18 06:44:35 -0700

    quicksort

---
+++ 22-quicksort/quicksort.mjs
@@ -0,0 +1,31 @@
+import {Comparator} from "../comparator.js";
+
+export default function quicksort(originalArray, comparatorCallback = null) {
+    const comparator = new Comparator(comparatorCallback);
+    const array = [...originalArray];
+
+    if (array.length <= 1) return array;
+
+    const leftArray = [];
+    const rightArray = [];
+
+    const pivotElement = array.shift();
+    const centerArray = [pivotElement];
+
+    // split all elements amongst left, right, center arrays
+    while (array.length) {
+        const currentElement = array.shift();
+        if (comparator.equal(currentElement, pivotElement)) {
+            centerArray.push(currentElement);
+        } else if (comparator.lessThan(currentElement, pivotElement)) {
+            leftArray.push(currentElement);
+        } else {
+            rightArray.push(currentElement);
+        }
+    }
+
+    const leftArraySorted = quicksort(leftArray, comparatorCallback);
+    const rightSortedArray = quicksort(rightArray, comparatorCallback);
+
+    return [...leftArraySorted, ...centerArray, ...rightSortedArray];
+}
